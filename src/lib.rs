be5bac284138a299da79fb3dda8af2ead890618a
jerry73204 <jerry73204@gmail.com>
2022-07-24 00:54:11 +0800

    Delete root node if it is marked deleted

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,18 @@ mod node;
 mod utils;
 
 use crate::node::Node;
-use crossbeam::epoch::{self, Guard};
+use crossbeam::epoch::{self, Atomic, Guard, Owned, Shared};
 use error::Error;
 use std::borrow::Borrow;
 use std::hash::Hash;
+use std::sync::atomic::Ordering::*;
 
 #[derive(Debug)]
 pub struct Trie<S, V>
 where
     S: Eq + Hash,
 {
-    root: Node<S, V>,
+    root: Atomic<Node<S, V>>,
 }
 
 impl<S, V> Trie<S, V>
@@ -23,13 +24,15 @@ where
     S: Eq + Hash,
 {
     pub fn new() -> Self {
-        Self { root: Node::new() }
+        Self {
+            root: Atomic::null(),
+        }
     }
 
     pub fn pin(&self) -> GuardedTrie<'_, S, V> {
         GuardedTrie {
             guard: epoch::pin(),
-            trie: self,
+            root: &self.root,
         }
     }
 }
@@ -49,7 +52,7 @@ where
     S: Eq + Hash,
 {
     guard: Guard,
-    trie: &'g Trie<S, V>,
+    root: &'g Atomic<Node<S, V>>,
 }
 
 impl<'g, S, V> GuardedTrie<'g, S, V>
@@ -62,7 +65,7 @@ where
         S: Borrow<Q>,
         Q: Hash + Eq + 'a,
     {
-        self.trie.root.get(key, &self.guard)
+        self.root()?.get(key, &self.guard)
     }
 
     pub fn insert<K>(&self, key: K, value: V) -> Option<&V>
@@ -83,7 +86,7 @@ where
     where
         K: IntoIterator<Item = S>,
     {
-        self.trie.root.insert(key, value, &self.guard)
+        self.get_or_create_root().insert(key, value, &self.guard)
     }
 
     pub fn remove<'a, Q, K>(&self, key: K) -> Option<&V>
@@ -107,15 +110,50 @@ where
         S: Borrow<Q>,
         Q: Hash + Eq + 'a,
     {
-        let (value, is_child_removed) = self.trie.root.remove(key, &self.guard)?;
+        let (value, is_child_removed) = self
+            .root()
+            .ok_or(Error::NotFound)?
+            .remove(key, &self.guard)?;
+
         if is_child_removed {
-            todo!();
+            self.root.store(Shared::null(), Release);
         }
 
         Ok(value)
     }
 
     pub fn iter(&'g self) -> Box<dyn Iterator<Item = &'g V> + 'g> {
-        self.trie.root.iter(&self.guard)
+        Box::new(
+            self.root()
+                .into_iter()
+                .flat_map(|root| root.iter(&self.guard)),
+        )
+    }
+
+    fn root(&self) -> Option<&Node<S, V>> {
+        let shared = self.root.load_consume(&self.guard);
+        unsafe { shared.as_ref() }
+    }
+
+    fn get_or_create_root(&self) -> &Node<S, V> {
+        match self.root() {
+            Some(root) => root,
+            None => {
+                let new_shared = Owned::new(Node::new()).into_shared(&self.guard);
+                let result = self.root.compare_exchange(
+                    Shared::null(),
+                    new_shared,
+                    AcqRel,
+                    Acquire,
+                    &self.guard,
+                );
+
+                let shared = match result {
+                    Ok(_) => new_shared,
+                    Err(error) => error.current,
+                };
+                unsafe { shared.deref() }
+            }
+        }
     }
 }

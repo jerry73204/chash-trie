7041a65891ad05eaaf6019ac6493c1aacc0da6fa
jerry73204 <jerry73204@gmail.com>
2022-07-23 12:04:06 +0800

    Use sharded-slab to allocate nodes

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,18 @@ mod utils;
 
 use crate::node::Node;
 use crossbeam::epoch::{self, Guard};
+use sharded_slab::Slab;
 use std::borrow::Borrow;
 use std::hash::Hash;
+use std::sync::Arc;
 
 #[derive(Debug)]
 pub struct Trie<S, V>
 where
     S: Eq + Hash,
 {
-    root: Node<S, V>,
+    root_id: NodeId,
+    slab: Arc<Slab<Node<S, V>>>,
 }
 
 impl<S, V> Trie<S, V>
@@ -19,13 +22,17 @@ where
     S: Eq + Hash,
 {
     pub fn new() -> Self {
-        Self { root: Node::new() }
+        let slab = Arc::new(Slab::new());
+        let root_id = NodeId(slab.insert(Node::new()).unwrap());
+
+        Self { root_id, slab }
     }
 
     pub fn pin(&self) -> GuardedTrie<'_, S, V> {
         GuardedTrie {
             guard: epoch::pin(),
-            trie: self,
+            root_id: self.root_id,
+            slab: &self.slab,
         }
     }
 }
@@ -45,7 +52,8 @@ where
     S: Eq + Hash,
 {
     guard: Guard,
-    trie: &'g Trie<S, V>,
+    root_id: NodeId,
+    slab: &'g Arc<Slab<Node<S, V>>>,
 }
 
 impl<'g, S, V> GuardedTrie<'g, S, V>
@@ -58,14 +66,14 @@ where
         S: Borrow<Q>,
         Q: Hash + Eq + 'a,
     {
-        self.trie.root.get(key, &self.guard)
+        self.root().get(key, self)
     }
 
     pub fn insert<K>(&self, key: K, value: V) -> Option<&V>
     where
         K: IntoIterator<Item = S>,
     {
-        self.trie.root.insert(key, value, &self.guard)
+        self.root().insert(key, value, self)
     }
 
     pub fn remove<'a, Q, K>(&self, key: K) -> Option<&V>
@@ -74,10 +82,52 @@ where
         S: Borrow<Q>,
         Q: Hash + Eq + 'a,
     {
-        self.trie.root.remove(key, None, &self.guard)
+        let (value, _) = self.root().remove(key, self)?;
+        Some(value)
     }
 
-    pub fn iter(&'g self) -> Box<dyn Iterator<Item = &'g V> + 'g> {
-        self.trie.root.iter(&self.guard)
+    // pub fn iter(&'g self) -> Box<dyn Iterator<Item = &'g V> + 'g> {
+    //     self.root().iter(self)
+    // }
+
+    fn root(&self) -> sharded_slab::Entry<'_, Node<S, V>> {
+        self.slab.get(self.root_id.0).unwrap()
+    }
+
+    // fn root_owned(&self) -> sharded_slab::OwnedEntry<Node<S, V>> {
+    //     self.slab.clone().get_owned(self.root_id.0).unwrap()
+    // }
+
+    fn get_node(&self, id: NodeId) -> Option<sharded_slab::Entry<'_, Node<S, V>>> {
+        self.slab.get(id.0)
+    }
+
+    fn create_node(&self) -> NodeId {
+        let id = self.slab.insert(Node::new()).unwrap();
+        NodeId(id)
+    }
+
+    fn remove_node(&self, id: NodeId) -> bool {
+        self.slab.remove(id.0)
     }
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct NodeId(pub usize);
+
+// #[derive(Debug)]
+// pub struct Entry<'g, S, V>
+// where
+//     S: Eq + Hash,
+// {
+//     entry: sharded_slab::Entry<'g, Node<S, V>>,
+// }
+
+// impl<'g, S, V> Entry<'g, S, V>
+// where
+//     S: Eq + Hash,
+// {
+//     pub fn value(&self) -> &V {
+//         self.entry
+//     }
+// }
--- src/node.rs
+++ src/node.rs
@@ -5,14 +5,17 @@ use std::iter;
 use std::sync::atomic::Ordering::*;
 use std::sync::RwLock;
 
-use crate::utils::{new_map, Map};
+use crate::{
+    utils::{new_map, Map},
+    GuardedTrie, NodeId,
+};
 
 #[derive(Debug)]
 pub(crate) struct Node<S, V>
 where
     S: Eq + Hash,
 {
-    pub(crate) children: Map<S, Atomic<Node<S, V>>>,
+    pub(crate) children: Map<S, NodeId>,
     pub(crate) value: Atomic<V>,
     pub(crate) is_deleted: RwLock<bool>,
 }
@@ -29,13 +32,14 @@ where
         }
     }
 
-    pub fn get<'a, 'g, Q, K>(&'g self, key: K, guard: &'g Guard) -> Option<&'g V>
+    pub fn get<'a, 'g, Q, K>(&self, key: K, trie: &'g GuardedTrie<S, V>) -> Option<&'g V>
     where
         K: IntoIterator<Item = &'a Q>,
         S: Borrow<Q>,
         Q: Hash + Eq + 'a,
     {
         let mut key = key.into_iter();
+        let guard = &trie.guard;
 
         // Get the value
         let value = match key.next() {
@@ -46,11 +50,10 @@ where
                         return None;
                     }
 
-                    let entry = self.children.get(seg)?;
-                    let atomic = entry.value();
-                    load_atomic(atomic, guard)?
+                    let child_id = *self.children.get(seg)?.value();
+                    trie.get_node(child_id).unwrap()
                 };
-                child_node.get(key, guard)?
+                child_node.get(key, trie)?
             }
             None => {
                 let is_deleted = self.is_deleted.read().unwrap();
@@ -65,11 +68,12 @@ where
         Some(value)
     }
 
-    pub fn insert<'g, K>(&'g self, key: K, value: V, guard: &'g Guard) -> Option<&'g V>
+    pub fn insert<'g, K>(&self, key: K, value: V, trie: &'g GuardedTrie<S, V>) -> Option<&'g V>
     where
         K: IntoIterator<Item = S>,
     {
         let mut key = key.into_iter();
+        let guard = &trie.guard;
 
         match key.next() {
             Some(seg) => {
@@ -78,14 +82,13 @@ where
                     if *is_deleted {
                         todo!("retry");
                     }
-                    let entry = self
+                    let child_id = *self
                         .children
                         .entry(seg)
-                        .or_insert_with(|| Atomic::new(Node::new()));
-                    let atomic = entry.value();
-                    load_atomic(atomic, guard)?
+                        .or_insert_with(|| trie.create_node());
+                    trie.get_node(child_id).unwrap()
                 };
-                ref_.insert(key, value, guard)
+                ref_.insert(key, value, trie)
             }
             None => {
                 let is_deleted = self.is_deleted.read().unwrap();
@@ -97,76 +100,64 @@ where
         }
     }
 
-    pub fn remove<'a, 'g, Q, K>(
-        &'g self,
-        key: K,
-        parent: Option<(&Self, &Q, Shared<'g, Self>)>,
-        guard: &'g Guard,
-    ) -> Option<&'g V>
+    pub fn remove<'a, 'g, Q, K>(&self, key: K, trie: &'g GuardedTrie<S, V>) -> Option<(&'g V, bool)>
     where
         K: IntoIterator<Item = &'a Q>,
         S: Borrow<Q>,
         Q: Hash + Eq + 'a,
     {
         let mut key = key.into_iter();
+        let guard = &trie.guard;
 
         // Get the value
-        let value = match key.next() {
+        let (value, is_self_deleted) = match key.next() {
             Some(seg) => {
                 // Find the related child
-                let shared = {
+                let (child_id, child_entry) = {
                     let is_deleted = self.is_deleted.read().unwrap();
                     if *is_deleted {
                         todo!("retry");
                     }
 
-                    let entry = self.children.get(seg)?;
-                    let atomic = entry.value();
-                    atomic.load_consume(guard)
+                    let child_id = *self.children.get(seg)?;
+                    let child_entry = trie.get_node(child_id).unwrap();
+                    (child_id, child_entry)
                 };
-                let child_node = unsafe { shared.as_ref() }?;
 
-                // Delete the value in descendents. During the
-                // process, the hash map entry for the child may be
-                // set to null.
-                let value = child_node.remove(key, Some((self, seg, shared)), guard)?;
+                // Delete the value in descendents.
+                let (value, is_child_deleted) = child_entry.remove(key, trie)?;
+                drop(child_entry);
 
-                {
+                let is_self_deleted = {
                     let mut is_deleted = self.is_deleted.write().unwrap();
 
                     // Check if some deleter else removes this node already.
                     if *is_deleted {
-                        return Some(value);
+                        return Some((value, false));
                     }
 
-                    // If the entry is set to null, remove it.
-                    if let Some(entry) = self.children.get(seg) {
-                        if load_atomic(entry.value(), guard).is_none() {
-                            self.children.remove(seg);
-                        }
+                    // If the child was deleted in this thread, remove
+                    // the corresponding entry.
+                    if is_child_deleted {
+                        self.children.remove(seg);
+                        let ok = trie.remove_node(child_id);
+                        assert!(ok);
                     }
 
                     // If the node has no children and the value is
                     // unset, mark his node deleted and delete the
                     // entry on parent to this node.
-                    if self.children.is_empty() && self.value.load_consume(guard).is_null() {
+                    let is_self_deleted =
+                        self.children.is_empty() && self.value.load_consume(guard).is_null();
+
+                    if is_self_deleted {
                         *is_deleted = true;
-
-                        if let Some((parent, prev_seg, expect_value)) = parent {
-                            if let Some(atomic) = parent.children.get(prev_seg) {
-                                let _ = atomic.compare_exchange(
-                                    expect_value,
-                                    Shared::null(),
-                                    AcqRel,
-                                    Acquire,
-                                    guard,
-                                );
-                            }
-                        }
                     }
-                }
 
-                value
+                    is_self_deleted
+                };
+
+                (value, is_self_deleted)
             }
             None => {
                 let mut is_deleted = self.is_deleted.write().unwrap();
@@ -183,48 +174,39 @@ where
                 // If this node has no children, ,mark this node
                 // deleted and set the entry on parent to this node to
                 // null.
-                if self.children.is_empty() {
+                let is_self_deleted = self.children.is_empty();
+                if is_self_deleted {
                     *is_deleted = true;
-
-                    if let Some((parent, prev_seg, expect_value)) = parent {
-                        if let Some(atomic) = parent.children.get(prev_seg) {
-                            let _ = atomic.compare_exchange(
-                                expect_value,
-                                Shared::null(),
-                                AcqRel,
-                                Acquire,
-                                guard,
-                            );
-                        }
-                    }
                 }
-                value
+
+                (value, is_self_deleted)
             }
         };
 
-        Some(value)
+        Some((value, is_self_deleted))
     }
 
-    pub fn iter<'g>(&'g self, guard: &'g Guard) -> Box<dyn Iterator<Item = &'g V> + 'g> {
-        let curr_value = iter::once_with(|| load_atomic(&self.value, guard)).flatten();
+    // pub fn iter<'g>(&self, trie: &'g GuardedTrie<S, V>) -> Box<dyn Iterator<Item = &'g V> + 'g> {
+    //     let guard = &trie.guard;
+    //     let curr_value = iter::once_with(|| load_atomic(&self.value, guard)).flatten();
 
-        let child_values = self.children.iter().flat_map(|entry| {
-            let child = entry.value();
-            let shared = child.load_consume(guard);
-            let ref_ = unsafe { shared.deref() };
-            ref_.iter(guard)
-        });
+    //     let child_values = self.children.iter().flat_map(|entry| {
+    //         let child = entry.value();
+    //         let shared = child.load_consume(guard);
+    //         let ref_ = unsafe { shared.deref() };
+    //         ref_.iter(trie)
+    //     });
 
-        let chain = curr_value.into_iter().chain(child_values);
-        Box::new(chain)
-    }
+    //     let chain = curr_value.into_iter().chain(child_values);
+    //     Box::new(chain)
+    // }
 
     // fn value<'g>(&'g self, guard: &'g Guard) -> Option<&'g V> {
     //     let shared = self.value.load_consume(guard);
     //     unsafe { shared.as_ref() }
     // }
 
-    fn set_value<'g>(&'g self, new_value: V, guard: &'g Guard) -> Option<&'g V> {
+    fn set_value<'g>(&self, new_value: V, guard: &'g Guard) -> Option<&'g V> {
         let new_value = Owned::new(new_value);
         let orig_shared = self.value.swap(new_value, AcqRel, guard);
         unsafe { orig_shared.as_ref() }
